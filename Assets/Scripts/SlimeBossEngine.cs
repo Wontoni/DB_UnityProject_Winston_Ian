1adf7262a1d81637a38904ce85231109c5607ad8
inaguu <ianjguani@gmail.com>
2023-11-24 17:44:06 -0800

    created gotHit function in slime and create slimeDuplicate

--- Assets/Scripts/SlimeBossEngine.cs
+++ Assets/Scripts/SlimeBossEngine.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SlimeBoss : MonoBehaviour
 {
+    [SerializeField] GameObject slimeboss;
+
+    [SerializeField] float jump;
+
     [Range(0f, 20f)]
     [SerializeField] float period = 1.0f;
 
@@ -15,10 +19,13 @@ public class SlimeBoss : MonoBehaviour
     private float nextActionTime = 0.0f;
     Animator animator;
 
+    PolygonCollider2D hitbox;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        hitbox = GetComponent<PolygonCollider2D>();
     }
 
     // Start is called before the first frame update
@@ -34,14 +41,17 @@ public class SlimeBoss : MonoBehaviour
         if (Time.time > nextActionTime)
         {
             nextActionTime += period;
-            Jump();
+            //Jump(hitbox);
+            gotHit();
         }
 
-        if (target)
+        chasePlayer();
+
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Green Jump - Jump Land"))
         {
-            Vector3 direction = (target.position - transform.position).normalized;
-            moveDirection = direction;
+            hitbox.GetComponent<PolygonCollider2D>().enabled = true;
         }
+
     }
 
     private void FixedUpdate()
@@ -52,9 +62,33 @@ public class SlimeBoss : MonoBehaviour
         }
     }
 
-    private void Jump()
+    private void Jump(PolygonCollider2D other)
     {
         animator.SetTrigger("isJumping");
+        other.GetComponent<PolygonCollider2D>().enabled = false;
+    }
+
+    private void gotHit()
+    {
+        animator.SetTrigger("isHurt");
+        transform.localScale = new Vector3(transform.localScale.x - 0.75f, transform.localScale.y - 1f); // max 5 times
+    }
+
+    private void SlimeDuplicate()
+    {
+        GameObject newSlimeBoss = Instantiate(slimeboss);
+        newSlimeBoss.transform.localScale = new Vector3(transform.localScale.x - 0.75f, transform.localScale.y - 1f);
+        newSlimeBoss.transform.position = transform.position;
+        newSlimeBoss.SetActive(true);
+    }
+
+    private void chasePlayer()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            moveDirection = direction;
+        }
     }
 }
 
